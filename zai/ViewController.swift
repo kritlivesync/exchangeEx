5d0267cc67b62a42e9e8b8ce41872b45b48e541c
kyotaw <kyotaw@users.noreply.github.com>
2016-08-18 23:56:32 +0900

    Initial Commit

---
+++ zai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zai
+//
+//  Created by 渡部郷太 on 8/18/16.
+//  Copyright © 2016 watanabe kyota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
